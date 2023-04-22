a03463b61c7e7cdfdc1c7a772b87d2d80f3156db
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-04-22 17:20:38 -0300

    UPDATE: Card styles

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -8,7 +8,15 @@ export function Card ({ recipe }) {
       <div className='card__instructions'>
         <p>{recipe.instructions}</p>
       </div>
-      <img src={recipe.imageUrl} alt={recipe.name} width='80px' height='80px' />
+      <div className='card__boxing'>
+        <div className='card__boxing-back' />
+        <img src={recipe.imageUrl} alt={recipe.name} width='80px' height='80px' />
+        <ul className='card__boxing__ingredients'>
+          {recipe.ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+      </div>
       <p><span className='bold'>Cooking time:</span> {recipe.cookingTime} minutes</p>
     </article>
   )
